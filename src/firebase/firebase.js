395174d5a9685d51d218b3efca461e91533c3939
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-27 20:05:04 +0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,16 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD1v7BtfjynQTbAPCTAumRV8b5ASp2sR4s",
-    authDomain: "react-todo-crud.firebaseapp.com",
-    databaseURL: "https://react-todo-crud.firebaseio.com",
-    projectId: "react-todo-crud",
-    storageBucket: "react-todo-crud.appspot.com",
-    messagingSenderId: "491813254587",
-    appId: "1:491813254587:web:cd48008f0f4c6a7ef5809e",
-    measurementId: "G-DZ8ZVC1Z6K"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
 };
+
 // Initialize Firebase
 let firebaseApp = firebase.initializeApp(firebaseConfig);
 
